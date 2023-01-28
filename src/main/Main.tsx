9a5f5245f140d22a93dd5b2ef221b8c37da85b6f
Chamunorwa Mutezva <ckmutezva@gmail.com>
2023-01-28 08:11:39 +0200

    fix: float display of zero after decimal point

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -15,6 +15,12 @@ const Main: FunctionComponent = () => {
   const { selectTheme } = useContext(DataContext);
   const [firstOperand, setFirstOperand] = useState<number>(0);
   const [secondOperand, setSecondOperand] = useState<number>(0);
+  const [firstOperandDisplay, setFirstOperandDisplay] = useState<string>(
+    firstOperand.toString()
+  );
+  const [secondOperandDisplay, setSecondOperandDisplay] = useState<string>(
+    secondOperand.toString()
+  );
   const [calc, setCalc] = useState<boolean>(false);
   const [selectOperator, setSelectOperator] = useState<string>("");
   const [result, setResult] = useState<number>(0);
@@ -35,23 +41,40 @@ const Main: FunctionComponent = () => {
     // maximum digits has been set to 12. Send notification to alert user
 
     if (display.length > 12) {
-      return  toast.info(`Limit of 12 has been reached `);
+      return toast.info(`Limit of 12 has been reached `);
     }
 
     if (display === "0" && id === 0) {
-      console.log(display, id);
       return;
     }
-    setDisplay(() => display + id);
+
+    if (display === "0") {
+      setDisplay(() => id.toString());
+    }
+
+    if (display !== "0") {
+      setDisplay(() => display + id);
+    }
     console.log(display);
     // calc is false at the start, any numbers entered when calc is false will be added to the
     // string display - converted to a number by setFirstOperand. When calc is true - triggered by
     // a click on any of the operators, then the collection of numbers will become secondOperand
     if (!calc) {
       setFirstOperand(() => Number(display + id));
+      if (display === "0") {
+        setFirstOperandDisplay(() => id.toString());
+      } else {
+        setFirstOperandDisplay(() => display + id.toString());
+      }
     } else {
       setSecondOperand(() => Number(display + id));
+      if (display === "0") {
+        setSecondOperandDisplay(() => id.toString());
+      } else {
+        setSecondOperandDisplay(() => display + id.toString());
+      }
     }
+    // displayStrings(id)
   }
 
   const handleOperations = (id: string) => {
@@ -80,8 +103,10 @@ const Main: FunctionComponent = () => {
       setDisplay((display) => display + id);
       if (!calc) {
         setFirstOperand((display) => Number(display + id));
+        setFirstOperandDisplay(() => display + id.toString());
       } else {
         setSecondOperand((display) => Number(display + id));
+        setSecondOperandDisplay(() => display + id.toString());
       }
     }
   };
@@ -91,9 +116,21 @@ const Main: FunctionComponent = () => {
     } else {
       setDisplay((display) => display.slice(0, -1));
       if (!calc) {
-        setFirstOperand(() => Number(display.slice(0, -1)));
+        if (display.length === 1) {
+          setFirstOperand(() => 0);
+          setFirstOperandDisplay(() => "0");
+        } else {
+          setFirstOperand(() => Number(display.slice(0, -1)));
+          setFirstOperandDisplay(() => firstOperandDisplay.slice(0, -1));
+        }
       } else {
-        setSecondOperand(() => Number(display.slice(0, -1)));
+        if (display.length === 1) {
+          setSecondOperand(() => 0);
+          setFirstOperandDisplay(() => "0");
+        } else {
+          setSecondOperand(() => Number(display.slice(0, -1)));
+          setSecondOperandDisplay(() => secondOperandDisplay.slice(0, -1));
+        }
       }
     }
   };
@@ -105,6 +142,11 @@ const Main: FunctionComponent = () => {
     setResult(() => 0);
     setOperator(() => "");
     setShowResult(() => false);
+    setDisplay(() => "0");
+    setFirstOperand(() => 0);
+    setSecondOperand(() => 0);
+    setFirstOperandDisplay(() => "0");
+    setSecondOperandDisplay(() => "0");
   };
 
   function clearCalculations() {
@@ -170,6 +212,8 @@ const Main: FunctionComponent = () => {
     return () => document.body.removeEventListener("keydown", handleKeyDown);
   });
 
+  useEffect(() => {}, [display]);
+
   return (
     <main>
       <div
@@ -184,15 +228,19 @@ const Main: FunctionComponent = () => {
         <div className="secondDisplay" aria-live="polite">
           {!calc
             ? ""
-            : firstOperand + " " + selectOperator + " " + secondOperand}
+            : firstOperandDisplay +
+              " " +
+              selectOperator +
+              " " +
+              secondOperandDisplay}
         </div>
 
         <div className="current" aria-live="polite">
           {calc
-            ? secondOperand
+            ? secondOperandDisplay
             : showResult
             ? result.toLocaleString("en")
-            : firstOperand}
+            : firstOperandDisplay}
         </div>
       </div>
       <div
